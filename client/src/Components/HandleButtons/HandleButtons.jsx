c4ef7215a355ca7bd9d121b8e1f0979e65930488
Denis <denis_jr2001@hotmail.com>
2022-05-22 11:23:07 -0300

    component with buttons delete and edit made

--- client/src/Components/HandleButtons/HandleButtons.jsx
+++ client/src/Components/HandleButtons/HandleButtons.jsx
@@ -17,7 +17,7 @@ const HandleButtons = ({ finalPath, idItem }) => {
     <>
       <button
         type="button"
-        class="btn btn-primary"
+        className="btn btn-primary"
         data-bs-toggle="modal"
         data-bs-target="#staticBackdrop"
       >
@@ -25,7 +25,7 @@ const HandleButtons = ({ finalPath, idItem }) => {
       </button>
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="staticBackdrop"
         data-bs-backdrop="static"
         data-bs-keyboard="false"
@@ -33,29 +33,29 @@ const HandleButtons = ({ finalPath, idItem }) => {
         aria-labelledby="staticBackdropLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
                 Modal title
               </h5>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">...</div>
-            <div class="modal-footer">
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
+                className="btn btn-secondary"
                 data-bs-dismiss="modal"
               >
                 Close
               </button>
-              <button type="button" class="btn btn-primary">
+              <button type="button" className="btn btn-primary">
                 Understood
               </button>
             </div>
