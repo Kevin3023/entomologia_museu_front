f6f4a9ab4839ec55d43c18fc941f88b5aa3dac0e
Denis <denis_jr2001@hotmail.com>
2022-05-28 19:36:47 -0300

    css renamed

--- client/src/Components/FormAddSingleCharacteristic/FormAddSingleCharacteristic.jsx
+++ client/src/Components/FormAddSingleCharacteristic/FormAddSingleCharacteristic.jsx
@@ -3,7 +3,7 @@ import { Formik, Form, Field} from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-import "./FormAddCharacteristics.css";
+import "./FormAddSingleCharacteristics.css";
 
 const FormAddCharacteristics = ({ finalPath, field, title, setObjectList}) => {
   const initialValues = {
