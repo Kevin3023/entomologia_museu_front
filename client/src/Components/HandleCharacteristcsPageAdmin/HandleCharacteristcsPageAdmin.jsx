607df36e983a6d94adbb3b4b6dc080c50c6432eb
¨Denis <denis_jr2001@hotmail.com>
2022-05-30 17:25:45 -0300

    using other concept for implement add e view

--- client/src/Components/HandleCharacteristcsPageAdmin/HandleCharacteristcsPageAdmin.jsx
+++ client/src/Components/HandleCharacteristcsPageAdmin/HandleCharacteristcsPageAdmin.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { Button, Modal } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import FormAddSingleCharacteristic from "../FormAddSingleCharacteristic/FormAddSingleCharacteristic";
 import FormAddTwoCharacteristics from "../FormAddTwoCharacteristcs/FormAddTwoCharacteristcs";
 import HandleButtons from "../HandleButtons/HandleButtons";
+import ModalEditItem from "../ModalEditItem/ModalEditItem";
 
 const HandleCharacteristcsPage = ({
   finalPath,
@@ -12,101 +14,72 @@ const HandleCharacteristcsPage = ({
   titleRelationship,
   finalRelationshipPath,
   nameRelationship,
-  tableName
+  tableName,
+  fields_,
 }) => {
-
-
   const [objectList, setObjectList] = useState([]);
-  const [dataList, setDataList] = useState([])
+  const [modalShow, setModalShow] = useState(false);
+  const [data, setData] = useState({});
+  const [nome, setNome] = useState("");
 
   const update = () => {
     axios
       .get(`https://api-museu-entomologiaufra.herokuapp.com/${finalPath}`)
+      .then((result) => {
+        setObjectList(result.data);
+      });
+  };
+
+  const handleDelete = id => {
+    axios
+    .delete(`https://api-museu-entomologiaufra.herokuapp.com/${finalPath}/${id}`)
+    .then(() => update());
+  }
+
+  const handleEdit = id => {
+    axios
+    .get(`https://api-museu-entomologiaufra.herokuapp.com/${finalPath}/${id}`)
     .then((result) => {
-      setObjectList(result.data);
+      setData(result.data);
+      setModalShow(true);
     });
+  }
+
+  const handleSave = id => {
     
+    setModalShow(false);
   }
 
   useEffect(update, [finalPath]);
 
   return (
-    <>
-      {fields ? (
-        <div className="container rounded border-secondary bg-light p-4 mt-5">
-          <FormAddTwoCharacteristics
-            finalPath={finalPath}
-            field={field}
-            title={title}
-            setObjectList={setObjectList}
-            titleRelationship={titleRelationship}
-            nameRelationship={nameRelationship}
-            finalRelationshipPath={finalRelationshipPath}
-          />
-          <table className="table table-striped table-hover">
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>{titleRelationship}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {objectList.map((element, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{element.nome}</td>
-                    <td>{element[tableName].nome}</td>
-                    <td className="d-flex justify-content-end">
-                      <HandleButtons
-                        finalPath={finalPath}
-                        idItem={element.id}
-                        update={() => update()}
-                        title={title}
-                        field={field}
-                      />
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <div className="container rounded border-secondary bg-light p-4 mt-5">
-          <FormAddSingleCharacteristic
-            finalPath={finalPath}
-            field={field}
-            title={title}
-            setObjectList={setObjectList}
-          />
-          <table className="table table-striped table-hover">
-            <thead>
-              <tr>
-                <th>Nome</th>
-              </tr>
-            </thead>
-            <tbody>
-              {objectList.map((element, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{element.nome}</td>
-                    <td className="d-flex justify-content-end">
-                      <HandleButtons
-                        finalPath={finalPath}
-                        idItem={element.id}
-                        update={() => update()}
-                        title={title}
-                        field={field}
-                      />
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+    <div className="container rounded border-secondary bg-light p-4 mt-5">
+      <ModalEditItem show={modalShow} title={title} content={<input onChange={event => setNome(event.target.value)} value={nome} />} handleClose={() => setModalShow(false)} handleSave={handleSave}/>
+      <Button variant="success">Adicionar</Button>
+      {objectList.length > 0 && (
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              {Object.keys(objectList[0]).filter(v => v !== 'id').map((v, k) => <th key={k}>{v.toUpperCase()}</th>)}
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {objectList.map((element, index) => {
+              return (
+                <tr key={index}>
+                  {Object.keys(element).filter(v => v !== 'id').map( (v,k) => <td key={k}>{element[v].nome ?? element[v]}</td>)}
+                  <td className="d-flex justify-content-end">
+                    <Button variant="primary" onClick={() => handleEdit(element.id)}>Editar</Button>
+                    <Button variant="danger" onClick={() => window.confirm("Deseja excluir este item?") && handleDelete(element.id)}>Excluir</Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       )}
-    </>
+    </div>
   );
 };
 
