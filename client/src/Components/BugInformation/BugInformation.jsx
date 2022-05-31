a32c8db1f56bd1414783f364960eee61af155a58
¨Denis <denis_jr2001@hotmail.com>
2022-05-31 16:54:35 -0300

    table data changed, added new fields

--- client/src/Components/BugInformation/BugInformation.jsx
+++ client/src/Components/BugInformation/BugInformation.jsx
@@ -2,45 +2,80 @@ import React from "react";
 import BugPic from "../../assets/img/vespa.jpg";
 import NavMain from "../NavMain/NavMain";
 
-const BugInformation = ({dataInsect}) => {
+const BugInformation = ({ data }) => {
   return (
     <>
-      <NavMain/>
+      <NavMain />
 
       <div className="card mb-3 w-50 mt-4 mx-auto bg-secondary">
-      <h3 className="text-white text-center mt-3">{dataInsect.nomeVulgar}</h3>
-        <img src={BugPic} className="card-img-top" alt={dataInsect.nomeVulgar} title={dataInsect.nomeVulgar} />
+        <h3 className="text-white text-center mt-3">{data.nome_comum}</h3>
+        <img
+          src={BugPic}
+          className="card-img-top"
+          alt={data.nome_comum}
+          title={data.nome_comum}
+        />
         <div className="card-body">
           <h5 className="card-title text-center text-white mt-1">
             Características Gerais
           </h5>
-        <div className="row row-cols-md-2 mx-auto">
-          <ul className="list-group mb-3">
-            <li className="list-group-item"><b>Abdomen:</b> {dataInsect.abdomen}</li>
-            <li className="list-group-item"><b>Antenas:</b> {dataInsect.antena}</li>
-            <li className="list-group-item"><b>Aparelho Bucal:</b> {dataInsect.aparelhoBucal}</li>
-            <li className="list-group-item"><b>Asas:</b> {dataInsect.asa}</li>
-            <li className="list-group-item"><b>Habitat:</b>{dataInsect.habitat}</li>
-            <li className="list-group-item"><b>Pernas:</b> {dataInsect.perna}</li>
-          </ul>
-          <ul className="list-group">
-            <li className="list-group-item"><b>Filo:</b> {dataInsect.filo}</li>
-            <li className="list-group-item"><b>Classe:</b> {dataInsect.classe}</li>
-            <li className="list-group-item"><b>Ordem:</b> {dataInsect.ordem}</li>
-            <li className="list-group-item"><b>Família:</b> {dataInsect.familia}</li>
-            <li className="list-group-item"><b>Gênero:</b> {dataInsect.genero}</li>
-            <li className="list-group-item"><b>Curiosidade:</b> {dataInsect.curiosidade}</li>
-          </ul>
-
-        </div>
+          <div className="row row-cols-md-2 mx-auto">
+            <ul className="list-group mb-3">
+              <li className="list-group-item">
+                <b>Nome Científico:</b> {data.nome_cientifico}
+              </li>
+              <li className="list-group-item">
+                <b>Nome Vulgar:</b> {data.nome_comum}
+              </li>
+              <li className="list-group-item">
+                <b>Filo:</b> {data.filo}
+              </li>
+              <li className="list-group-item">
+                <b>Classe:</b> {data.classe}
+              </li>
+              <li className="list-group-item">
+                <b>Ordem:</b>
+                {data.ordem}
+              </li>
+              <li className="list-group-item">
+                <b>Família:</b> {data.familia}
+              </li>
+              <li className="list-group-item">
+                <b>Gênero:</b> {data.genero}
+              </li>
+              <li className="list-group-item">
+                <b>Comportamento:</b> {data.comportamento}
+              </li>
+            </ul>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <b>Aparelho Bucal:</b> {data.aparelho_bucal}
+              </li>
+              <li className="list-group-item">
+                <b>Asa:</b> {data.asa}
+              </li>
+              <li className="list-group-item">
+                <b>Abdomên:</b> {data.abdomen}
+              </li>
+              <li className="list-group-item">
+                <b>Antenas:</b> {data.antena}
+              </li>
+              <li className="list-group-item">
+                <b>Metamorfose:</b> {data.metamorfose}
+              </li>
+              <li className="list-group-item">
+                <b>Perna:</b> {data.perna}
+              </li>
+              <li className="list-group-item">
+                <b>Habitat:</b> {data.habitat}
+              </li>
+            </ul>
+          </div>
 
           <h5 className="card-title text-white text-center mt-2">
             Curiosidade
           </h5>
-          <p className="card-text text-white">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
+          <p className="card-text text-white">{data.curiosidade}</p>
         </div>
       </div>
     </>
