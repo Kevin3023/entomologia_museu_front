079b0663b963c079b5d952a57539421b953f9a7b
¨Denis <denis_jr2001@hotmail.com>
2022-06-01 17:37:49 -0300

    formating data

--- client/src/Components/Forms/FiloForm.jsx
+++ client/src/Components/Forms/FiloForm.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
 
-const FiloForm = ({data}) => {
-    return ( <>
-        <h1>{data.nome}</h1>
-    </> );
-}
- 
+const FiloForm = ({ data, title, handleClose, handleSave }) => {
+  useEffect(() => {
+    console.log(data);
+  }, []);
+
+  const handleData = (e) => {
+    e.preventDefault();
+
+
+    handleSave()
+  };
+
+  return (
+    <>
+      <form /*onSubmit={handleData}*/>
+        <label className="form-label fs-5">{title}</label>
+        <input
+          className="form-control"
+          type="text"
+          name={data.nome || title}
+          placeholder={`Inserir ${title} aqui`}
+          /* onChange={handleInputChange} */
+          value={data.nome || ""}
+        />
+        <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+            </Button>
+            <Button variant="primary" type="submit">
+            Salvar
+            </Button>
+
+        </Modal.Footer>
+      </form>
+    </>
+  );
+};
+
 export default FiloForm;
