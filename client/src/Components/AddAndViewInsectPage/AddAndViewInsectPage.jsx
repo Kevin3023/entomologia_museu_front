82cc350100a37eb55d2facc976365c5d71dc7fcd
¨Denis <denis_jr2001@hotmail.com>
2022-06-09 16:57:43 -0300

    formik removed

--- client/src/Components/AddAndViewInsectPage/AddAndViewInsectPage.jsx
+++ client/src/Components/AddAndViewInsectPage/AddAndViewInsectPage.jsx
@@ -5,38 +5,7 @@ const AddAndViewInsectPage = () => {
         <div className="container rounded border-secondary bg-light p-4 mt-5">
         
         alou
-        {/* <Formik
-          initialValues={initialValues}
-          onSubmit={onSubmit}
-          validationSchema={validationSchema}
-        >
-          {(formik) => (
-            <Form> */}
-              {/* {console.log(formik)} */}
-  
-              {/* <label className="mb-1">Metamorfose</label>
-              <Field
-                className="form-control mb-3"
-                name={field1}
-                placeholder="Insira a metamorfose aqui..."
-               />
-  
-              <label className="mb-1">Descrição</label>
-              <br />
-              <Field
-                className="form-control mb-3"
-                name={field2}
-                placeholder="Insira a descrição aqui..."
-              />
-  
-              <div className="modal-footer">
-                <button type="submit" className="btn btn-primary">
-                  Salvar
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik> */}
+        
   
         {/* <table className="table table-striped table-hover">
           <thead>
