5a723fb54da67f43d0f162320dbc035c2ea82d89
¨Denis <denis_jr2001@hotmail.com>
2022-06-10 14:40:40 -0300

    default page admin

---
+++ client/src/Components/DefaultTutorialPageAdmin/DefaultTutorialPageAdmin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DefaultTutorialPageAdmin = () => {
+    return ( <div className="container rounded border-secondary bg-light p-4 mt-5">
+        <h2 className='text-center' >Como usar o sistema</h2>
+        <p>Para estar <b>visualizando</b>, <b>alterando</b> ou <b>adicionando</b> novas informacoes, basta clicar em um dos botoes de navegacao que automaticamente ira abrir uma lista de propriedade que voce pode estar tratando</p>
+        <h4 className='text-center'>Exemplo</h4>
+        <p className='text-center'>Taxonomia &gt; Filo</p>
+    </div>
+    );
+}
+ 
+export default DefaultTutorialPageAdmin;
