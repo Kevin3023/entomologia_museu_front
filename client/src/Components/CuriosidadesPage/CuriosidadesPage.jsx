8fc8553c77f77b0d3f1ecad6f5b61fb4038cd217
Denis <denis_jr2001@hotmail.com>
2022-06-11 20:35:48 -0300

    footer added

--- client/src/Components/CuriosidadesPage/CuriosidadesPage.jsx
+++ client/src/Components/CuriosidadesPage/CuriosidadesPage.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import NavMain from '../NavMain/NavMain';
-import bug from "../../assets/img/besourinho.jpg"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import NavMain from "../NavMain/NavMain";
+import bug from "../../assets/img/besourinho.jpg";
+import FooterMainCopy from "../FooterMain copy/FooterMainCopy";
 
-const CuriosidadesPage = ({data}) => {
-    
-    let navigate = useNavigate()
-    
-    return ( <>
-    <NavMain/>
+const CuriosidadesPage = ({ data }) => {
+  let navigate = useNavigate();
 
-    {/* especies just with curiosity */}
+  return (
+    <>
+      <NavMain />
 
-    <div className="container mt-4">
+      {/* especies just with curiosity */}
+
+      <div className="container mt-4">
         <div className="row row-cols-1 row-cols-md-3">
-            {
-                data.map((inseto, index) => {
-                    return(
-          <div key={index} className="col mb-5" onClick={() => {
-              navigate(`/${inseto.nomeVulgar}`)
-            }}>
-            <div className="card">
-              <img src={bug} className="card-img-top" alt="imagem do inseto" title="inseto"/>
-              <div className="card-body">
-                <h5 className="card-title text-center mt-3"><b>{inseto.nomeVulgar}</b></h5>
-                <p className="card-text">
-                {inseto.curiosidade}
-                </p>
+          {data.map((inseto, index) => {
+            return (
+              <div
+                key={index}
+                className="col mb-5"
+                onClick={() => {
+                  navigate(`/${inseto.nomeVulgar}`);
+                }}
+              >
+                <div className="card">
+                  <img
+                    src={bug}
+                    className="card-img-top"
+                    alt="imagem do inseto"
+                    title="inseto"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-center mt-3">
+                      <b>{inseto.nomeVulgar}</b>
+                    </h5>
+                    <p className="card-text">{inseto.curiosidade}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>)
-                })
-            }
-
+            );
+          })}
         </div>
       </div>
+      <FooterMainCopy className="fixed-bottom"/>
+    </>
+  );
+};
 
-    </> );
-}
- 
 export default CuriosidadesPage;
