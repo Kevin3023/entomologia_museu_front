f8422a543e6df2e6c34650877d094f722bdbea37
Denis <denis_jr2001@hotmail.com>
2022-06-12 16:45:03 -0300

    search bug implemented

--- client/src/Components/FormSearchBug/FormSearchBug.jsx
+++ client/src/Components/FormSearchBug/FormSearchBug.jsx
@@ -1,29 +1,78 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import bug from "../../assets/img/louvaDeusGrande.jpeg";
 
-
-const FormSearchBug = ({ handleSubmit, handleInputChange, inputValue }) => {
+const FormSearchBug = ({
+  handleSubmit,
+  handleInputChange,
+  inputValue,
+  dataSearch,
+}) => {
+  let navigate = useNavigate();
+  
   return (
-    <div className="navbar navbar-expand-lg navbar-light bg-light mt-5 justify-content-center py-3">
-      <form onSubmit={handleSubmit}>
-        <div className="input-group mb-3 ">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Nome do Inseto..."
-            name="input-common-user"
-            onChange={handleInputChange}
-            value={inputValue}
-          />
-          <button
-            className="btn btn-outline-secondary">
-            Pesquisar
-          </button>
-        </div>
-        <div id="passwordHelpBlock" className="form-text">
-          Insira o nome do inseto para a página mostrar as informacões do mesmo.
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="navbar navbar-expand-lg navbar-light bg-light mt-5 justify-content-center py-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group mb-3 ">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Nome do Inseto..."
+              name="input-common-user"
+              onChange={handleInputChange}
+              value={inputValue}
+            />
+            <button className="btn btn-outline-secondary">Pesquisar</button>
+          </div>
+          <div id="passwordHelpBlock" className="form-text">
+            Insira o nome do inseto para a página mostrar as informacões do
+            mesmo.
+          </div>
+        </form>
+      </div>
+
+      <div className="mt-5 d-flex align-items-center justify-content-center w-100">
+        {dataSearch.length ? <div className="row row-cols-1 row-cols-md-3">
+          {dataSearch.map((inseto, index) => {
+            return (
+              <div
+                key={index}
+                className="col mb-5"
+                onClick={() => {
+                  navigate(`/${inseto.nome_comum}`);
+                }}
+              >
+                <div className="card">
+                  <img
+                    src={bug}
+                    className="card-img-top"
+                    alt="imagem do inseto"
+                    title={inseto.nome_comum}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <b>{inseto.nome_comum}</b>
+                    </h5>
+                    <p className="card-text">
+                      <b>Nome Científico:</b> <i>{inseto.nome_cientifico}</i>
+                    </p>
+                    <p className="card-text">
+                      <b>Habitat:</b> {inseto.habitat.nome}
+                    </p>
+                    <p className="card-text">
+                      <b>Metamorfose:</b> {inseto.metamorfose.nome}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div> : <div>Nada encontrado</div>}
+        
+        
+      </div>
+    </>
   );
 };
 
